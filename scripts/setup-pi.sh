2abf1457e50d88648edb1d4671dc40351d4a347f
Jared Irby <jared@irbygroup.com>
2026-04-03 23:37:59 -0500

    Build RTKLIB outside tmpfs

--- scripts/setup-pi.sh
+++ scripts/setup-pi.sh
@@ -105,7 +105,7 @@ install_rtklib() {
     fi
 
     info "Building RTKLIB str2str from pinned source (${version})..."
-    tmpdir="$(mktemp -d)"
+    tmpdir="$(mktemp -d /var/tmp/survey365-rtklib.XXXXXX)"
     srcdir="$tmpdir/RTKLIB-2.5.0"
     curl -fsSL "$archive_url" | tar -xz -C "$tmpdir"
     make --directory="$srcdir/app/consapp/str2str/gcc"
